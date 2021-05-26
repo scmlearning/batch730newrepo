ec41c00fb37ca589d74ed90786611e4877151b4a
root <root@ip-172-31-44-42.ap-south-1.compute.internal>
2021-05-26 02:14:52 +0000

    part of my new 730 batch

---
+++ Home.java
@@ -0,0 +1,7 @@
+class Home
+{
+public static void main(String []args)
+{
+Sysyem.out.println("Outout coming from Home.java");
+}
+}
---
+++ Login.java
@@ -0,0 +1,7 @@
+class Login
+{
+public static void main(String []args)
+{
+Sysyem.out.println("Outout coming from Login.java");
+}
+}
